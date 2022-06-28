14fea9bff4a437eb460d4cf7db153765d85ff103
Furqan <furqantesting1994@gmail.com>
2022-06-28 18:58:44 +0500

    Main Dev

--- PracticeProject/Controllers/ViewController.swift
+++ PracticeProject/Controllers/ViewController.swift
@@ -55,8 +55,7 @@ class ViewController: UIViewController {
     }
     
     func newTestGitFunction(){
-        print("Hello Git. I am in Dev branch3.")
-        print("Hello Git. I am in Dev branch.")
+        print("Hello Git. I am in Main branch.")
     }
     
     
